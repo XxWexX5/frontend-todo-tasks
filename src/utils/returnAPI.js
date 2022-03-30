4fb23c59890d1b69b0522cb5f2849693915cb27f
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-03-30 12:23:04 -0300

    [ FEATURE ] Adding variable env.

--- src/utils/returnAPI.js
+++ src/utils/returnAPI.js
@@ -1,5 +1,3 @@
 export function returnAPI() {
-  return process.env.NODE_ENV === 'development'
-    ? process.env.REACT_APP_URL_LOCAL_API
-    : process.env.REACT_APP_URL_API;
+  return process.env.REACT_APP_URL_API;
 }
